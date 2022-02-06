77522a0ff0f1881f98cdda0bcb8394e52a0c2bbc
andreahbji <andrewpetrovic@gmail.com>
2022-02-06 21:07:45 +0800

    setup foreground bubble

--- app-start/src/main/java/com/example/android/people/data/NotificationHelper.kt
+++ app-start/src/main/java/com/example/android/people/data/NotificationHelper.kt
@@ -143,6 +143,12 @@ class NotificationHelper(private val context: Context) {
                         icon
                     )
                     .setDesiredHeightResId(R.dimen.bubble_height)
+                    .apply {
+                        if (fromUser) {
+                            setAutoExpandBubble(true)
+                            setSuppressNotification(true)
+                        }
+                    }
                     .build()
             )
             .setContentTitle(chat.contact.name)
